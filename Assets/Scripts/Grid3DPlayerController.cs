47225b28c545d4ec95ee0f2f04490ede81b5f42a
Ric <ricardoduennas@gmail.com>
2024-07-24 16:14:44 -0500

    Player update to grow trees only on soil and change soil texture when done

--- Assets/Scripts/Grid3DPlayerController.cs
+++ Assets/Scripts/Grid3DPlayerController.cs
@@ -15,6 +15,7 @@ public class Grid3DPlayerController : MonoBehaviour
     public GameObject treePrefab;
     public GameObject trenchPrefab;
     public Material grassMaterial;
+    public Grid gridObject;
     // Variables privadas para el movimiento
     private bool isMoving = false;              // Indica si el jugador está en movimiento
     private Vector3 targetPos;                 // Posición objetivo del movimiento
@@ -25,6 +26,7 @@ public class Grid3DPlayerController : MonoBehaviour
     // Diccionarios para almacenar árboles y trincheras
     public Dictionary<Vector3, GameObject> PlantedTrees { get; private set; } = new Dictionary<Vector3, GameObject>();
     public Dictionary<Vector3, GameObject> BuiltTrenches { get; private set; } = new Dictionary<Vector3, GameObject>();
+    public Cell[,] gridCells;
 
     void Start()
     {
@@ -32,6 +34,9 @@ public class Grid3DPlayerController : MonoBehaviour
 
         targetPos = transform.position;
         targetRot = transform.rotation;
+
+        gridObject = GameObject.Find("Grid").GetComponent<Grid>();
+        gridCells = gridObject.grid;
 
     }
 
@@ -56,9 +61,9 @@ public class Grid3DPlayerController : MonoBehaviour
         }
         MoveTowardsTarget();
 
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.P))
             PlantTree();
-        else if (Input.GetKeyDown(KeyCode.R))
+        else if (Input.GetKeyDown(KeyCode.O))
             BuildTrench();
 
         // Manejo de animaciones adicionales
@@ -124,7 +129,7 @@ public class Grid3DPlayerController : MonoBehaviour
     {
         Vector3 position = GetGridPosition();
 
-        if (!PlantedTrees.ContainsKey(position))
+        if (!PlantedTrees.ContainsKey(position) && gridCells[(int)position.x, (int)position.z].isSoil)
         {
             GameObject tree = Instantiate(treePrefab, position, Quaternion.identity);
             AudioManager.Instance.PlaySFX(plantingAudioClip, 0.7f);
@@ -133,7 +138,10 @@ public class Grid3DPlayerController : MonoBehaviour
             // Asegurarse de que el árbol no se destruya automáticamente
             DontDestroyOnLoad(tree);
 
-            ChangeGroundMaterial(position, grassMaterial);
+            gridCells[(int)position.x, (int)position.z].isSoil = false;    
+            gridObject.swapSquare(gridCells, (int)position.x, (int)position.z, false);
+
+//            ChangeGroundMaterial(position, grassMaterial);
 
 
         }
